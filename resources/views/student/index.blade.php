f601745aa9951b8bba20168a99ca1e3c8b2380fa
Atikur Rahaman <laravelDeveloper01862@gmail.com>
2022-09-21 17:45:08 +0600

    partial end

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -222,9 +222,9 @@ student result
     <div class="col-md-12 text-center mt-5">
         <a href="" class="btn btn-sm btn-danger print_btn"><i class="fa fa-print" aria-hidden="true"></i> Print</a>
     </div>
-    <div class="row" id="certificate">
-        <div class="col-md-1"></div>
-        <div class="parent col-md-10  mt-5" >
+    <div class="row" id="">
+        <div class="col-md-2"></div>
+        <div class="parent col-md-8 mt-5" >
             {{--  <p>This is div</p>  --}}
             <div  class="elder_child" style="padding: 10px;background-image: url({{asset('assets/img/certificateBg.png')}});">
                    <div class="row" >
@@ -245,38 +245,38 @@ student result
                                     NAME OF STUDENT
                                </div>
                                <div class="col-md-7">
-                                   : <span id="s_name" style="font-weight: 800"></span>
+                                   : <span class="s_name" style="font-weight: 800"></span>
                                </div>
 
                             <div class="col-md-5">
                                 FATHER&#8217S NAME
                             </div>
                             <div class="col-md-7">
-                                : <span id="f_name"  style="font-weight: 800"></span>
+                                : <span class="f_name"  style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
                                 MOTHER&#8217S NAME
                             </div>
                             <div class="col-md-7">
-                                : <span id="m_name"  style="font-weight: 800"></span>
+                                : <span class="m_name"  style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
                                DATE OF BIRTH
                             </div>
                             <div class="col-md-7">
-                                : <span id="date_of_birth"  style="font-weight: 800"></span>
+                                : <span class="date_of_birth"  style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
                                 ID NO.
                             </div>
                             <div class="col-md-7">
-                                : <span id="roll_no" style="font-weight: 800></span">
+                                : <span class="roll_no" style="font-weight: 800></span">
                             </div>
                             <div class="col-md-5">
                             REG NO.
                             </div>
                             <div class="col-md-7">
-                                : <span id="reg_no" style="font-weight: 800"></span>
+                                : <span class="reg_no" style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
                             NAME OF COURSE
@@ -285,16 +285,16 @@ student result
                                 : <span class="course_name" style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
-                                DURATION OF COURSE
+                                 COURSE DURATION
                             </div>
                             <div class="col-md-7">
-                                : <span id="course_duration"  style="font-weight: 800"></span>
+                                : <span class="course_duration"  style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
-                               DURATION
+                               PASSING YEAR
                             </div>
                             <div class="col-md-7">
-                                : <span id="passing_year"  style="font-weight: 800"></span>
+                                : <span class="passing_year"  style="font-weight: 800"></span>
                             </div>
                             <div class="col-md-5">
                                LETTER GRADE
@@ -373,22 +373,17 @@ student result
                                 <thead>
                                     <th>SL NO</th>
                                     <th>COURSE NAEM</th>
-                                    <th>POINT</th>
                                     <th>GRADE POINT</th>
+                                    <th>GPA</th>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <td>1</td>
                                         <td class="course_name"></td>
-                                        <td id="cgpa"></td>
+                                        <td class="cgpa"></td>
                                         <td rowspan="3" class="letter_grade"></td>
                                     </tr>
-                                    {{--  <tr>
-                                        <td>2</td>
-                                        <td>Graphics Design </td>
-                                        <td>4.00</td>
 
-                                    </tr>  --}}
                                 </tbody>
                             </table>
                         </div>
@@ -413,8 +408,208 @@ student result
                    </div>
             </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="col-md-2"></div>
     </div>
+</div>
+
+
+
+<div class="row d-none" id="certificate" style="height: 1500px">
+    {{--  <div class="col-md-"></div>  --}}
+    <div class="parent col-md-11 mx-auto mt-5">
+        <div  class="elder_child" style="padding: 10px;background-image: url({{asset('assets/img/certificateBg.png')}});">
+            <div class="row">
+                 <div class="col-md-12 text-center m-2">
+                     <p > Govt. of the peoples  Republic Bagladesh TEchnical Education Board AFfiliated Institution</p>
+                     <h1 style="font-weight: 500;">DEWAN ICT INSTITUTE </h1>
+                     <H4 style="font-weight: bold;" class="text-danger">Institute Code : 50679</H4>
+                     <p style="font-weight: bold">Address : 20-21, 374, Mukto Bangla Complex <br>
+                         (3rd & 4th Floor) Mirpur-1, Dhaka-1216, Bangladesh</p>
+
+                         <div class="btn btn-danger" style="margin-top:50px;margin-bottom:100px;">ACADEMIC TRANSCRIPT</div>
+                 </div>
+
+                 <div class="col-md-7 mx-auto" style="border:2px solid black" >
+
+                     <div class="row mt-2" style="padding: 15px">
+                         <div class="col-md-5">
+                             NAME OF STUDENT
+                        </div>
+                        <div class="col-md-7">
+                            : <span class="s_name" style="font-weight: 800"></span>
+                        </div>
+
+                     <div class="col-md-5">
+                         FATHER&#8217S NAME
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="f_name"  style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                         MOTHER&#8217S NAME
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="m_name"  style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                        DATE OF BIRTH
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="date_of_birth"  style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                         ID NO.
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="roll_no" style="font-weight: 800></span">
+                     </div>
+                     <div class="col-md-5">
+                     REG NO.
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="reg_no" style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                     NAME OF COURSE
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="course_name" style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                          COURSE DURATION
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="course_duration"  style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                        PASSING YEAR
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="passing_year"  style="font-weight: 800"></span>
+                     </div>
+                     <div class="col-md-5">
+                        LETTER GRADE
+                     </div>
+                     <div class="col-md-7">
+                         : <span class="letter_grade"  style="font-weight: 800"></span>
+                     </div>
+                    </div>
+                 </div>
+
+                 {{--  mark range   --}}
+                 <div class="col-md-4 mx-auto">
+                 <table  class="text-center gradig_table"  >
+                         <thead>
+                           <tr>
+                             <th style="" width:'25%' scope="col">Range Of Mark</th>
+                             <th   scope="col">Grade</th>
+                             <th  scope="col">Point</th>
+                           </tr>
+                         </thead>
+                         <tbody>
+                           <tr>
+                             <td   >80 to Avobe</td>
+                             <td  >A+</td>
+                             <td  >4.00</td>
+                           </tr>
+                           <tr>
+                             <td   >75 - below 80</td>
+                             <td  >A</td>
+                             <td  >3.75</td>
+                           </tr>
+                           <tr>
+                             <td   >70 - below 75</td>
+                             <td  >A-</td>
+                             <td  >3.50</td>
+                           </tr>
+                           <tr>
+                             <td   >65 - below 70</td>
+                             <td  >B+</td>
+                             <td  >3.25</td>
+                           </tr>
+                           <tr>
+                             <td   >60 - below 65</td>
+                             <td  >B</td>
+                             <td  >3.00</td>
+                           </tr>
+                           <tr>
+                             <td   >55 - below 60</td>
+                             <td  >B-</td>
+                             <td  >2.75</td>
+                           </tr>
+                           <tr>
+                             <td   >50 - below 55</td>
+                             <td  >C</td>
+                             <td  >2.50</td>
+                           </tr>
+                           <tr>
+                             <td   >45 - below 50</td>
+                             <td  >D</td>
+                             <td  >2.00</td>
+                           </tr>
+                           <tr>
+                             <td   > below 40</td>
+                             <td  >F</td>
+                             <td  >0.00</td>
+                           </tr>
+
+
+                         </tbody>
+                       </table>
+                 </div>
+
+                 {{--  grade point  --}}
+                 <div class="col-md-12" style="margin-top: 50px;">
+                     <table class="cgpa_table text-center">
+                         <thead>
+                             <th>SL NO</th>
+                             <th>COURSE NAEM</th>
+                             <th>GRADE POINT</th>
+                             <th>GPA</th>
+                         </thead>
+                         <tbody>
+                             <tr>
+                                 <td>1</td>
+                                 <td class="course_name"></td>
+                                 <td class="cgpa"></td>
+                                 <td rowspan="3" class="letter_grade"></td>
+                             </tr>
+                             <tr>
+                                <td>2</td>
+                                <td>Tanvir</td>
+                                <td>A</td>
+                             </tr>
+                             <tr>
+                                <td>2</td>
+                                <td>Tanvir</td>
+                                <td>A</td>
+                             </tr>
+
+                         </tbody>
+                     </table>
+                 </div>
+                </div>
+
+                <div class="col-md-12" style="margin-top: 470PX">
+                    <div class="row "  >
+                        <div class="col-md-4" >
+                            <h4 style="text-decoration:overline">DIRECTOR</h4>
+                        </div>
+                        <div class="col-md-4" align="center">
+                            <h4 style="text-decoration:overline">Exam Controller</h4>
+                        </div>
+                        <div class="col-md-4" align='right' >
+                            <h4 style="text-decoration:overline">CHAIRMAN</h4>
+                        </div>
+
+                        <div class="col-md-12 text-center mt-2">
+                           <small> This Academic Transcript may be verified at www.dewanict.com using the Academic Transcript holer registratio number</small>
+                        </div>
+                    </div>
+                </div>
+     </div>
+    </div>
+    {{--  <div class="col-md-2"></div>  --}}
 </div>
 
 
@@ -478,17 +673,17 @@ student result
                     if(response.success){
                         $('#student_result').removeClass('d-none');
                         $('#invalid_reg').addClass('d-none');
-                        $('#s_name').text(response.success.s_name);
-                        $('#m_name').text(response.success.m_name);
-                        $('#f_name').text(response.success.f_name);
+                        $('.s_name').text(response.success.s_name);
+                        $('.m_name').text(response.success.m_name);
+                        $('.f_name').text(response.success.f_name);
                         $('.course_name').text(response.success.course_name);
-                        $('#course_duration').text(response.success.course_duration);
-                        $('#passing_year').text(response.success.passing_year);
-                        $('#institute_name').text(response.success.institute);
-                        $('#date_of_birth').text(response.success.date_of_birth);
-                        $('#roll_no').text(response.success.roll_no);
-                        $('#reg_no').text(response.success.reg_number);
-                        $('#cgpa').text(response.success.cgpa);
+                        $('.course_duration').text(response.success.course_duration);
+                        $('.passing_year').text(response.success.passing_year);
+                        $('.institute_name').text(response.success.institute);
+                        $('.date_of_birth').text(response.success.date_of_birth);
+                        $('.roll_no').text(response.success.roll_no);
+                        $('.reg_no').text(response.success.reg_number);
+                        $('.cgpa').text(response.success.cgpa);
 
                         if(response.success.cgpa == 4 ) {
                             $('.letter_grade').text('A+');
@@ -524,10 +719,13 @@ student result
                     }
                 }
             });
+
             });
 
             $(document).on('click', '.print_btn', function(e){
                 e.preventDefault();
+                {{--  $('.parent').addClass('col-md-11');  --}}
+                $('#certificate').removeClass('d-none');
                 $("#certificate").print({
                      //Use Global styles
                      globalStyles : true,
@@ -545,7 +743,8 @@ student result
                      append : "",
                      pageTitle:"Dewan ICT",
                      //Log to console when printing is done via a deffered callback
-                     deferred: $.Deferred().done(function() { console.log('Printing done', arguments);
+                     deferred: $.Deferred().done(function() {
+                        $('#certificate').addClass('d-none');
                 })
 
             });
